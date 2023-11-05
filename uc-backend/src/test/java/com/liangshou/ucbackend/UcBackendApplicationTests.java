35c6f4c7cee05d6c414f8f82c9c3c3c801217c8b
LiangshouX <BIT_kiwiSean@163.com>
2023-11-05 15:36:46 +0800

    Initialized frontend and backend

---
+++ uc-backend/src/test/java/com/liangshou/ucbackend/UcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangshou.ucbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
